c957eabf3b678dd4ea44a3d349acc7da89e7fef5
gururaj02 <ngururaj58@gmail.com>
2025-02-06 07:47:30 +0530

    Added Initial Files

---
+++ E-commerce/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using E_commerce.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
---
+++ E-commerce/Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using E_commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // This will helps to create table in DB (Products is the name of the table in DB)
+        public DbSet<Product> Products { get; set; }
+    }
+}
