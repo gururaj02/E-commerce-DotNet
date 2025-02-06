c7a4dea539e9eb670df34787b9f98c150cf95fa4
gururaj02 <ngururaj58@gmail.com>
2025-02-06 23:11:22 +0530

    New Funtionality Added - Saving product in DB

--- E-commerce/Controllers/ProductsController.cs
+++ E-commerce/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using E_commerce.Services;
+﻿using E_commerce.Models;
+using E_commerce.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_commerce.Controllers
@@ -6,16 +7,65 @@ namespace E_commerce.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment environment;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             this.context = context;
+            this.environment = environment;
         }
 
         public IActionResult Index()
         {
             var products = context.Products.OrderByDescending(p => p.Id).ToList();
             return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if(productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The image file is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
+
+            // Svae image
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+            string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            // Save the new product in the DB
+            Product product = new Product()
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFile = newFileName,
+                CreatedAt = DateTime.Now,
+            };
+
+            // Saving in Products table
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Products");
         }
     }
 }
---
+++ E-commerce/Models/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce.Models
+{
+    public class ProductDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Description { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+    }
+}
